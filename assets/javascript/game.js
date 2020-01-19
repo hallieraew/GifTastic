ae652e4668c3a3ed246386044807170651768598
hallieraew <hallieweintraub@gmail.com>
2020-01-18 22:02:47 -0600

    images populating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,75 @@
+var topic = ["happy", "sad", "joy", "anger", "furious", "amazed", "amused", "embarassed", "bored", "confused", "excited", "scared", "shocked"];
+
+console.log(topic);
+
+function createButtons() {
+
+    $("#buttonsHere").empty();
+
+    for (var i = 0; i < topic.length; i++) {
+
+        var a = $("<button>");
+        a.addClass("mood");
+        a.attr("data-name", topic[i]);
+        a.text(topic[i]);
+
+        $("#buttonsHere").append(a);
+    }
+}
+
+createButtons();
+
+
+$("button").on("click", function () {
+    var mood = $(this).attr("data-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RGoL0T73mBwrnkpTnYLyZtUI1FQEuyhR&q=" + mood + "&limit=25&offset=0&rating=G&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        .then(function (response) {
+
+            var results = response.data;
+
+            for (var i = 0; i <= 10; i++) {
+
+                var gifDiv = $("<div>");
+
+                var moodImage = $("<img>");
+
+                // moodImage.attr("src", results[i].images.fixed_height.url);
+
+                moodImage.attr("src", results[i].images.fixed_height_still.url);
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                gifDiv.append(moodImage);
+                gifDiv.append(p);
+
+                $("#gifsHere").prepend(gifDiv);
+            }
+        })
+
+    })
+            // $("#gifsHere").on("click", function() {
+    
+            //     var state = $(this).attr("src", results[i].images.fixed_height_still.url);
+    
+            //     if (moodImage === state) {
+            //       $(this).attr($(this).attr("src", results[i].images.fixed_height.url));
+            //     } else {
+            //       $(this).attr("src", $(this).attr("data-still"));
+            //       $(this).attr("data-state", "still");
+            //     }
+            //   });
+// var mood = set inside the click function for generating gifs i.e.
+
+
 // need array of strings for category to cover with buttons - maybe mood? happy, sad, angry, curious, disgusted etc.
 // set var for mood
 // var for storing giphy url
