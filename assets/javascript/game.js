7f37c38d0b4fa43db58bcaf51c126c06f2242922
hallieraew <hallieweintraub@gmail.com>
2020-07-06 23:53:13 -0500

    updating style to match other projects and be more user friendly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,108 @@
-var topic = ["happy", "sad", "joy", "anger", "furious", "amazed", "amused", "embarassed", "bored", "confused", "excited", "scared", "shocked"];
+var topic = [
+	"Happy",
+	"Sad",
+	"Joy",
+	"Anger",
+	"Furious",
+	"Amazed",
+	"Amused",
+	"Embarassed",
+	"Bored",
+	"Confused",
+	"Excited",
+	"Scared",
+	"Shocked"
+];
 
 console.log(topic);
 
 function createButtons() {
+	$("#buttonsHere").empty();
 
-    $("#buttonsHere").empty();
+	for (var i = 0; i < topic.length; i++) {
+		var a = $("<button>");
+		a.addClass("btn btn-outline-dark mood");
+		a.attr("data-mood", topic[i]);
+		a.text(topic[i]);
 
-    for (var i = 0; i < topic.length; i++) {
-
-        var a = $("<button>");
-        a.addClass("mood");
-        a.attr("data-mood", topic[i]);
-        a.text(topic[i]);
-
-        $("#buttonsHere").append(a);
-    }
+		$("#buttonsHere").append(a);
+	}
 }
 
 createButtons();
 
-$("#userSearch").on("click", function (event) {
+$("#userSearch").on("click", function(event) {
+	event.preventDefault();
 
-    event.preventDefault();
+	var userSearch = $("#userInput")
+		.val()
+		.trim();
+	var userInput = userSearch;
 
-    var userSearch = $("#userInput").val().trim();
-    var userInput = userSearch;
+	topic.push(userInput);
 
-    topic.push(userInput);
-
-    gifSearch(userInput);
-    // $("#userInput").val("");
-    createButtons();
-
+	gifSearch(userInput);
+	// $("#userInput").val("");
+	createButtons();
 });
 
-function gifSearch(mood){
+function gifSearch(mood) {
+	var queryURL =
+		"https://api.giphy.com/v1/gifs/search?api_key=RGoL0T73mBwrnkpTnYLyZtUI1FQEuyhR&q=" +
+		mood +
+		"&limit=25&offset=0&rating=G&lang=en";
+	console.log(queryURL);
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.then(function(response) {
+		var results = response.data;
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=RGoL0T73mBwrnkpTnYLyZtUI1FQEuyhR&q=" + mood + "&limit=25&offset=0&rating=G&lang=en";
-    console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    
-        .then(function (response) {
-    
-            var results = response.data;
-    
-            for (var i = 0; i <= 10; i++) {
-    
-                var gifDiv = $("<div>");
-    
-                var moodImage = $("<img>");
-    
-                var moodImageStill = results[i].images.fixed_height_still.url
-    
-                var moodImageAnimate = results[i].images.fixed_height.url
-    
-                moodImage.addClass("gif");
-                moodImage.attr(
-                    {
-                        "src": moodImageStill,
-                        "data-still": moodImageStill,
-                        "data-animate": moodImageAnimate,
-                        "data-state": "still"
-                    }
-                )
-    
-                var rating = results[i].rating;
-    
-                var p = $("<p>").text("Rating: " + rating);
-    
-                gifDiv.append(moodImage);
-                gifDiv.append(p);
-    
-                $("#gifsHere").prepend(gifDiv);
-            }
-    
-        });
-        
-    }
-            $(document).on("click", ".gif", function () {
-                console.log("clicked")
-    
-                state = $(this).attr("data-state");
-    
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).data("still"));
-                    $(this).attr("data-state", "still");
-                }
-            });
+		for (var i = 0; i <= 10; i++) {
+			var gifDiv = $("<div>");
 
+			var moodImage = $("<img>");
 
-$(document).on("click", ".mood", function () {
-    var mood = $(this).attr("data-mood")
+			var moodImageStill = results[i].images.fixed_height_still.url;
 
-    gifSearch(mood);
+			var moodImageAnimate = results[i].images.fixed_height.url;
+
+			moodImage.addClass("gif");
+			moodImage.attr({
+				src: moodImageStill,
+				"data-still": moodImageStill,
+				"data-animate": moodImageAnimate,
+				"data-state": "still"
+			});
+
+			var rating = results[i].rating;
+
+			var p = $("<p>").text("Rating: " + rating);
+
+			gifDiv.append(moodImage);
+			gifDiv.append(p);
+
+			$("#gifsHere").prepend(gifDiv);
+		}
+	});
+}
+$(document).on("click", ".gif", function() {
+	console.log("clicked");
+
+	state = $(this).attr("data-state");
+
+	if (state === "still") {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	} else {
+		$(this).attr("src", $(this).data("still"));
+		$(this).attr("data-state", "still");
+	}
+});
+
+$(document).on("click", ".mood", function() {
+	var mood = $(this).attr("data-mood");
+
+	gifSearch(mood);
 });
